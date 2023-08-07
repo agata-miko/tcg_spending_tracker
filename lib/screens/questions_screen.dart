51b18f894444a4f2d3612d786d02d343bf129f38
mikol <mikolajek.a@gmail.com>
2023-08-07 11:46:16 +0200

    Module 73 - question index managed as a state

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -10,19 +10,30 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(width: double.infinity,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(currentQuestion.text, style: TextStyle(color: Colors.white),),
-          const SizedBox(height: 30.0),
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(answerText: answer, onPressed: () {});
-          }),
-        ],
+      child: Container(margin: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(currentQuestion.text, style: TextStyle(color: Colors.white), textAlign: TextAlign.center,),
+            const SizedBox(height: 30.0),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(answerText: answer, onPressed: answerQuestion);
+            }),
+          ],
+        ),
       ),
     );
   }
